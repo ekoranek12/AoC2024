f1c4715ab498676c84e638c87102055e3e0323dc
Eddie Koranek <eddie.koranek.12@gmail.com>
2024-12-01 12:51:34 -0600

    Move Day1 function out of playground for easier testing

---
+++ Advent of Code 2024/Day 1/Day1.swift
@@ -0,0 +1,79 @@
+//
+//  Day1.swift
+//  Advent of Code 2024
+//
+//  Created by Eddie Koranek on 12/1/24.
+//
+
+public typealias ListPair = (lhs: [Int], rhs: [Int])
+
+public enum Day1 {
+    public static func load() throws(FileLoader.LoadingError) -> String {
+        try FileLoader.load(resource: "Day1PuzzleInput", withExtension: "txt")
+    }
+
+    public static func parse(_ input: String) -> ListPair {
+        input
+            .split(separator: "\n")
+            .map { row in
+                let split = row.split(separator: " ", omittingEmptySubsequences: true)
+                return (split.first, split.last)
+            }
+            .compactMap { (lhs: (any StringProtocol)?, rhs: (any StringProtocol)?) -> (Int, Int)? in
+                guard let lhs, let rhs,
+                      let first = Int(lhs),
+                      let last = Int(rhs)
+                else {
+                    return nil
+                }
+
+                return (first, last)
+            }
+            .reduce(([Int](), [Int]())) { partialResult, pair in
+                (
+                    partialResult.0 + [pair.0],
+                    partialResult.1 + [pair.1]
+                )
+            }
+    }
+
+    public static func sort(_ input: ListPair) -> ListPair {
+        (
+            input.lhs.sorted(),
+            input.rhs.sorted()
+        )
+    }
+
+    public static func calculateDifference(_ input: ListPair) -> Int {
+        zip(input.lhs, input.rhs)
+            .reduce(0) { partialResult, pair in
+                return partialResult + abs(pair.1 - pair.0)
+            }
+    }
+
+    public static func countElements(_ input: [Int]) -> [Int: Int] {
+        // Speeds calculateSimilarity up from 3.831035137176514 → 0.0003800392150878906
+        var counter: [Int: Int] = [:]
+
+        for value in input {
+            let count = counter[value] ?? 0
+            counter[value] = count + 1
+        }
+
+        return counter
+    }
+
+    public static func calculateSimilarity(_ input: ListPair) -> Int {
+        var score = 0
+
+        let counts = countElements(input.rhs)
+
+        for value in input.lhs {
+            let count = counts[value] ?? 0
+
+            score += (value * count)
+        }
+
+        return score
+    }
+}
--- Advent of Code 2024/Day 1/Day1ScratchPad.playground/Contents.swift
+++ Advent of Code 2024/Day 1/Day1ScratchPad.playground/Contents.swift
@@ -1,106 +1,14 @@
 import Foundation
 
-typealias ListPair = (lhs: [Int], rhs: [Int])
+do {
+    let value = try Day1.load()
+    let pairs = Day1.parse(value)
+    let sortedPairs = Day1.sort(pairs)
+    let difference = Day1.calculateDifference(sortedPairs)
 
-let input = Bundle.main.url(forResource: "Day1PuzzleInput", withExtension: "txt")
-let data = try? Data(contentsOf: input!)
-let string = String(data: data!, encoding: .utf8)
-
-enum LoadingError: Error {
-    case missingFile
-    case malformedData(_ error: Error)
-    case invalidContents
-}
-
-func loadInputs() throws(LoadingError) -> String {
-    guard let input = Bundle.main.url(forResource: "Day1PuzzleInput", withExtension: "txt") else {
-        throw .missingFile
+    let similarity = measureDuration("Similarity") {
+        Day1.calculateSimilarity(sortedPairs)
     }
-
-    let data: Data
-    do {
-        data = try Data(contentsOf: input)
-    } catch {
-        throw .malformedData(error)
-    }
-
-    guard let string = String(data: data, encoding: .utf8) else {
-        throw .invalidContents
-    }
-
-    return string
+} catch {
+    print(error)
 }
-
-func parse(_ input: String) -> ListPair {
-    input
-        .split(separator: "\n")
-        .map { row in
-            let split = row.split(separator: " ", omittingEmptySubsequences: true)
-            return (split.first, split.last)
-        }
-        .compactMap { (lhs: (any StringProtocol)?, rhs: (any StringProtocol)?) -> (Int, Int)? in
-            guard let lhs, let rhs,
-                  let first = Int(lhs),
-                  let last = Int(rhs)
-            else {
-                return nil
-            }
-
-            return (first, last)
-        }
-        .reduce(([Int](), [Int]())) { partialResult, pair in
-            (
-                partialResult.0 + [pair.0],
-                partialResult.1 + [pair.1]
-            )
-        }
-}
-
-func sort(_ input: ListPair) -> ListPair {
-    (
-        input.lhs.sorted(),
-        input.rhs.sorted()
-    )
-}
-
-func calculateDifference(_ input: ListPair) -> Int {
-    zip(input.lhs, input.rhs)
-        .reduce(0) { partialResult, pair in
-            return partialResult + abs(pair.1 - pair.0)
-        }
-}
-
-func countElements(_ input: [Int]) -> [Int: Int] {
-    var counter: [Int: Int] = [:]
-
-    for value in input {
-        let count = counter[value] ?? 0
-        counter[value] = count + 1
-    }
-
-    return counter
-}
-
-func calculateSimilarity(_ input: ListPair) -> Int {
-    var score = 0
-
-    let counts = countElements(input.rhs)
-
-    for value in input.lhs {
-        let count = counts[value] ?? 0
-
-        score += (value * count)
-    }
-
-    return score
-}
-
-let value = try! loadInputs()
-let pairs = parse(value)
-let sortedPairs = sort(pairs)
-//let difference = calculateDifference(sortedPairs)
-
-let similarity = measureDuration("Similarity") {
-    calculateSimilarity(sortedPairs)
-}
-// 25,574,739, 3.831035137176514 → 0.02319812774658203
